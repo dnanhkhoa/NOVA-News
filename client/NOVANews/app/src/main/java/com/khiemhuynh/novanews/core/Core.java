2c75aae2a4c1505deb84ffcf54426c17f06f883d
khiemhuynh95 <khiemhuynh95@gmail.com>
2016-10-30 06:41:33 +0700

    demo 3

--- client/NOVANews/app/src/main/java/com/khiemhuynh/novanews/core/Core.java
+++ client/NOVANews/app/src/main/java/com/khiemhuynh/novanews/core/Core.java
@@ -35,7 +35,7 @@ public class Core {
         // Logic
 
         // Fake data
-        Category cat1 = new Category("TIN NÓNG");
+        Category cat1 = new Category("TIN NÓNG.");
         cat1.addItem(new Item("Không khởi tố vụ nam hành khách đánh nữ nhân viên hàng không", "Sự việc hai nam hành khách đánh nữ nhân viên hàng không tại sân bay Nội Bài diễn ra ngày 18/10. Đến ngày 20/10, Thủ tướng yêu cầu Chủ tịch UBND thành phố Hà Nội chỉ đạo các cơ quan chức năng khẩn trương làm rõ và báo cáo kết quả lên Thủ tướng trong tháng 10."));
         cat1.addItem(new Item("Hành khách kể về phút hoảng loạn trên phi cơ bốc cháy Mỹ ", "Chiếc phi cơ chở 161 hành khách và tổ bay 9 người khởi hành từ sân bay O'Hare, Chicago, bang Illinois, đến Miami, bang Florida, bốc cháy trên đường băng khi chuẩn bị cất cánh. 20 người bị thương nhẹ đang được điều trị trong bệnh viện. "));
         cat1.addItem(new Item("Bai 3", "Noi dung 3"));
@@ -47,14 +47,14 @@ public class Core {
         cat1.addItem(new Item("Bai 9", "Noi dung 3"));
         categories.add(cat1);
 
-        Category cat2 = new Category("TIN CHÍNH TRỊ");
-        cat2.addItem(new Item("Bai 1", "Noi dung 1"));
+        Category cat2 = new Category("TIN GIÁO DỤC.");
+        cat2.addItem(new Item("Lý do học tiếng Anh nhiều nhưng không tiến bộ ", "Nhiều học sinh chưa xác định đúng mục tiêu và lựa chọn chương trình không phù hợp với năng lực của bản thân. "));
         cat2.addItem(new Item("Bai 2", "Noi dung 2"));
         cat2.addItem(new Item("Bai 3", "Noi dung 3"));
         categories.add(cat2);
 
-        Category cat3 = new Category("TIN THỂ THAO");
-        cat3.addItem(new Item("Bai 1", "Noi dung 1"));
+        Category cat3 = new Category("TIN THỂ THAO.");
+        cat3.addItem(new Item("10 cầu thủ U19 được triệu tập vào đội U22 Việt Nam ", "Đội U22 quốc gia vừa được thành lập để dự giải giao hữu tại Trung Quốc có hơn một phần ba quân số vừa thi đấu thành công tại giải U19 châu Á."));
         cat3.addItem(new Item("Bai 2", "Noi dung 2"));
         cat3.addItem(new Item("Bai 3", "Noi dung 3"));
         categories.add(cat3);
