9c9b88c341a62fcd5f53b88fef1aa13edc7a42e5
khiemhuynh95 <khiemhuynh95@gmail.com>
2016-10-29 20:36:23 +0700

    Nearly Finished front-end

--- client/NOVANews/app/src/main/java/com/khiemhuynh/novanews/adapter/ColorAdapter.java
+++ client/NOVANews/app/src/main/java/com/khiemhuynh/novanews/adapter/ColorAdapter.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.khiemhuynh.novanews.R;
@@ -24,15 +23,11 @@ public final class ColorAdapter extends BaseAdapter {
     public ColorAdapter(Context context) {
         mInflater = LayoutInflater.from(context);
 
-        mItems.add(new Item("Red",       R.drawable.red));
-        mItems.add(new Item("Magenta",   R.drawable.magenta));
-        mItems.add(new Item("Dark Gray", R.drawable.dark_gray));
-        mItems.add(new Item("Gray",      R.drawable.gray));
-        mItems.add(new Item("Green",     R.drawable.green));
-        mItems.add(new Item("Cyan",      R.drawable.cyan));
+        mItems.add(new Item("Purple",0xffba68c8));
+        mItems.add(new Item("Pink", 0xfff06292));
+//        mItems.add(new Item("Blue", 0xff64b5f6));
+//        mItems.add(new Item("Cyan", 0xff4dd0e1));
 
-        mItems.add(new Item("Red",       R.drawable.red));
-        mItems.add(new Item("Magenta",   R.drawable.magenta));
     }
 
     @Override
@@ -46,40 +41,49 @@ public final class ColorAdapter extends BaseAdapter {
     }
 
     @Override
-    public long getItemId(int i) {
-        return mItems.get(i).drawableId;
+    public long getItemId(int position) {
+        return 0;
     }
+
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         View v = view;
-        ImageView picture;
+        TextView color, color2,color3;
         TextView name;
 
         if (v == null) {
             v = mInflater.inflate(R.layout.grid_item, viewGroup, false);
-            v.setTag(R.id.picture, v.findViewById(R.id.picture));
+            v.setTag(R.id.colorView, v.findViewById(R.id.colorView));
+
             v.setTag(R.id.text, v.findViewById(R.id.text));
         }
 
-        picture = (ImageView) v.getTag(R.id.picture);
+        color = (TextView) v.getTag(R.id.colorView);
+
         name = (TextView) v.getTag(R.id.text);
+
 
         Item item = getItem(i);
 
-        picture.setImageResource(item.drawableId);
+        color.setBackgroundColor(item.colorNum);
+        color.setText("-Phim Học đường Là Anh là dự án phim dài tập của FAPtv trong năm 2016 - phim được chăm chút kĩ lưỡng , kịch bản được phát triển bởi Hi Team" +
+                "Phim dự kiến phát sóng mỗi tuần 1 tập");
+        color.setTextColor(item.colorNum);
+
+
         name.setText(item.name);
 
         return v;
     }
 
-    private static class Item {
+    class Item {
         public final String name;
-        public final int drawableId;
+        public final int colorNum;
 
-        Item(String name, int drawableId) {
+        Item(String name, int colorNum) {
             this.name = name;
-            this.drawableId = drawableId;
+            this.colorNum = colorNum;
         }
     }
 }
