46a4ea3d7a64357bee8c2b3e13af8f569ba6c6c1
khiemhuynh95 <khiemhuynh95@gmail.com>
2016-11-06 10:16:12 +0700

    Added splash screen

---
+++ client/NOVANews/app/src/main/java/com/khiemhuynh/novanews/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.khiemhuynh.novanews;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(500);
+                } catch (Exception e) {
+
+                } finally {
+                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+}
